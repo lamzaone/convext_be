fb4a2cc1b3c53e5d882269a84be24734a9d982ff
Denis <denisdenis009@gmail.com>
2024-10-17 03:24:33 +0300

    - Add database configuration and models - Add fastapi setup - Add uvicorn run command

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, LargeBinary
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
