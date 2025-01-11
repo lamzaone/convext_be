26ace8cf2e11057d82476bbf1734dd403c6e8235
Ferdinand1201 <133119759+Ferdinand1201@users.noreply.github.com>
2025-01-11 14:06:41 +0200

    Compress .png for better optimization. Mp3 conversion revised. Docx conversion added.
    Testing is needed

--- convert.sh
+++ convert.sh
@@ -41,15 +41,15 @@ CONV_PATH_WITH_NAME="$CONV_PATH/$CONV_NAME"
 case "$TO_EXT" in
     # Docs
 
-    # doc/docx not working for now
-    # !!!THIS NEEDS TO BE CHANGED!!!
-    # "docx")
-    #     soffice --headless --infilter="writer_pdf_import" \
-    #         --convert-to doc:"writer_pdf_Export" $ORG_PATH \
-    #         --outdir "convfiles/" >/dev/null 2>&1
-    #     mv "convfiles/$ORG_NAME_NO_EXT.docx" "convfiles/$CONV_PATH.docx"
-    #     # echo -n "$CONV_PATH.docx" 
-    #    ;;
+    # doc/docx not sure if working
+    
+    "docx")
+        soffice --headless --convert-to docx "$FILE_PATH" \
+            --outdir "$CONV_PATH" >/dev/null 2>&1
+        mv "$CONV_PATH/$FILE_NAME_NO_EXT.docx" "$CONV_PATH_WITH_NAME.docx"
+        echo -n "$CONV_PATH_WITH_NAME.docx"
+        ;;
+
         
     "compdf")
         gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen \
@@ -65,31 +65,29 @@ case "$TO_EXT" in
         ;;
 
     # Images
-    # TODO: Maybe rethink file conversion by adding compression. Converted pngs
-    # get really big.
-    "jpeg"|"png"|"webp")
+    
+    "jpeg"|"webp")
         convert "$FILE_PATH" -quality 90 -colorspace sRGB \
             "$CONV_PATH_WITH_NAME.$TO_EXT"
-        # pngquant -f --ext .png "$CONV_PATH_WITH_NAME.$TO_EXT"
+        echo -n "$CONV_PATH_WITH_NAME.$TO_EXT"
+        ;;
+    
+    "png")
+        # Convert the PNG file and optionally compress using pngquant for better size optimization
+        convert "$FILE_PATH" -quality 90 -colorspace sRGB \
+            "$CONV_PATH_WITH_NAME.$TO_EXT"
+        pngquant -f --ext .png "$CONV_PATH_WITH_NAME.$TO_EXT" >/dev/null 2>&1
         echo -n "$CONV_PATH_WITH_NAME.$TO_EXT"
         ;;
 
     # Audio
-    "mp3@320k")
-        ffmpeg  -i "$FILE_PATH" -c:v libmp3lame -b:a 320k \
-            "$CONV_PATH_WITH_NAME.mp3"
+    "mp3@320k"|"mp3@256k"|"mp3@128k")
+        BITRATE=${TO_EXT#*@}
+        ffmpeg -i "$FILE_PATH" -c:a libmp3lame -b:a "$BITRATE" \
+            "$CONV_PATH_WITH_NAME.mp3" >/dev/null 2>&1
         echo -n "$CONV_PATH_WITH_NAME.mp3"
         ;;
-    "mp3@256k")
-        ffmpeg -i "$FILE_PATH" -c:v libmp3lame -b:a 256k \
-            "$CONV_PATH_WITH_NAME.mp3"
-        echo -n "$CONV_PATH_WITH_NAME.mp3"
-        ;;
-    "mp3@128k")
-        ffmpeg -i "$FILE_PATH" -c:v libmp3lame -b:a 128k \
-            "$CONV_PATH_WITH_NAME.mp3"
-        echo -n "$CONV_PATH_WITH_NAME.mp3"
-        ;;
+        
     "opus@128k")
         ffmpeg -i "$FILE_PATH" -c:v opus -b:a 128k \
             "$CONV_PATH_WITH_NAME.opus"
@@ -110,6 +108,7 @@ case "$TO_EXT" in
 
     # If none of the above, echo an error message.
     *)
+        echo "Error: Unsupported conversion format '$TO_EXT'."
         echo -n "-1"
         ;;
 esac
